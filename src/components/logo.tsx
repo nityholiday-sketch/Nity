6b93e0c2eea1450c52246072a3c2dfdfa03f8627
Bhupesh Bohara <nityholiday@gmail.com>
2025-08-28 17:18:23 +0000

    remove name "NityHoliday" near logo increse logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,13 +7,9 @@ export function Logo() {
       <Image
         src="https://firebasestorage.googleapis.com/v0/b/nityholiday-adventures.firebasestorage.app/o/logo.png?alt=media&token=bec7bdc9-a77b-45ab-810e-74676d691a4e"
         alt="Nityholiday Logo"
-        width={36}
-        height={36}
+        width={48}
+        height={48}
       />
-      <span className="ml-3 text-2xl font-bold tracking-tight">
-        <span style={{ color: '#2C3E50', fontFamily: 'cursive' }}>Nity</span>
-        <span style={{ color: '#F26D21', fontFamily: 'cursive' }}>Holiday</span>
-      </span>
     </div>
   );
 }
